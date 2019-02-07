f0e9688a91e47f6ed7fcf78fc50ebd18b20c47f7
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2019-02-07 10:50:14 +0100

    second

--- src/Complexnumber.java
+++ src/Complexnumber.java
@@ -1,2 +1,18 @@
 public class Complexnumber {
+    double a;
+    double b;
+
+public Complexnumber(double a, double b){
+    this.a=a;
+    this.b=b;
+    }
+public Complexnumber add(Complexnumber z2){
+    double a =this.a + z2.a;
+    double b= this.b + z2.b;
+    Complexnumber z3 = new Complexnumber(a,b);
+    return z3;
+    }
+public String toString(){
+    return this.a + "+" + this.b + "i";
+    }
 }
--- src/testComplexnumber.java
+++ src/testComplexnumber.java
@@ -1,2 +1,7 @@
 public class testComplexnumber {
+    public static void main(String[]args){
+        Complexnumber z1 = new Complexnumber(2,4);
+        Complexnumber z2= new Complexnumber(4,3);
+        z1.add(z2);
+    }
 }
