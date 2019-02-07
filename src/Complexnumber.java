359b2beb108a6172945017c108048217ab76be9a
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2019-02-07 11:12:04 +0100

    third

--- src/Complexnumber.java
+++ src/Complexnumber.java
@@ -6,12 +6,26 @@ public Complexnumber(double a, double b){
     this.a=a;
     this.b=b;
     }
+
 public Complexnumber add(Complexnumber z2){
     double a =this.a + z2.a;
     double b= this.b + z2.b;
     Complexnumber z3 = new Complexnumber(a,b);
     return z3;
     }
+
+public Complexnumber multiply(Complexnumber z2) {
+    double a = this.a * z2.a;
+    double b = this.b * z2.b;
+
+    double c = this.a * z2.b;
+    double d = this.b * z2.a;
+    Complexnumber z4 = new Complexnumber(a - b, c + d);
+    return z4;
+}
+
+
+    @Override
 public String toString(){
     return this.a + "+" + this.b + "i";
     }
--- src/testComplexnumber.java
+++ src/testComplexnumber.java
@@ -2,6 +2,9 @@ public class testComplexnumber {
     public static void main(String[]args){
         Complexnumber z1 = new Complexnumber(2,4);
         Complexnumber z2= new Complexnumber(4,3);
-        z1.add(z2);
+
+        Complexnumber z3= z1.add(z2);
+        System.out.println(z3);
+
     }
 }
